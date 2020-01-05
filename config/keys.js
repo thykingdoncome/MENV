cc0cc52153a84751ebeff214dd31527ae15053b7
israel david <davidisrael194@gmail.com>
2020-01-05 21:47:56 +0100

    heroku deploy

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoUrl: 'mongodb+srv://menvstack:menvstack@menvapp-s7tdv.mongodb.net/test',
+        mongoUrl: 'mongodb+srv://menvstack:menvstack@menvapp-s7tdv.mongodb.net/test?retryWrites=true&w=majority',
         secret: 'mysecret'
     }
 } else {
